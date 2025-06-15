674419f0d380854c6de9e4270ac9f63e71b90e46
Wagner Couto <wagnereler@hotmail.com>
2025-06-15 18:48:07 +0000

    fix configurações relaconada ao Cors

--- biblioteca-api/src/main/java/com/wagner/biblioteca/config/CorsConfig.java
+++ biblioteca-api/src/main/java/com/wagner/biblioteca/config/CorsConfig.java
@@ -1,4 +1,18 @@
 package com.wagner.biblioteca.config;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")               // para todas as rotas
+                .allowedOriginPatterns("*")      // libera todas as origens
+                .allowedMethods("*")             // libera todos os métodos (GET, POST, PUT…)
+                .allowedHeaders("*")             // libera todos os headers
+                .allowCredentials(true);         // mantém cookies e auth se precisar
+    }
 }
